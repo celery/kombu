5beed9d000b36ba4dcbbcabcd97804fc55dae569
Ask Solem <askh@opera.com>
2010-11-12 11:34:13 +0100

    Added functional tests for django-kombu

---
+++ kombu/tests/test_functional/test_django.py
@@ -0,0 +1,28 @@
+from nose import SkipTest
+
+from kombu.tests.test_functional import transport
+from kombu.tests.utils import redirect_stdouts
+
+
+class test_django(transport.TransportCase):
+    transport = "django"
+    prefix = "django"
+    event_loop_max = 10
+
+    def before_connect(self):
+
+        @redirect_stdouts
+        def setup_django(stdout, stderr):
+            try:
+                import djkombu
+            except ImportError:
+                raise SkipTest("django-kombu not installed")
+            from django.conf import settings
+            if not settings.configured:
+                settings.configure(DATABASE_ENGINE="sqlite3",
+                                   DATABASE_NAME=":memory:",
+                                   INSTALLED_APPS=("djkombu", ))
+            from django.core.management import call_command
+            call_command("syncdb")
+
+        setup_django()
