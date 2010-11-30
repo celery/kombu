0e44a16230266524ca0427116d4c5bef60bc765e
Ask Solem <askh@opera.com>
2010-11-30 19:56:39 +0100

    Bumped version to 0.9.7

--- README.rst
+++ README.rst
@@ -2,7 +2,7 @@
  kombu - AMQP Messaging Framework for Python
 #############################################
 
-:Version: 0.9.6
+:Version: 0.9.7
 
 Synopsis
 ========
--- kombu/__init__.py
+++ kombu/__init__.py
@@ -1,5 +1,5 @@
 """AMQP Messaging Framework for Python"""
-VERSION = (0, 9, 6)
+VERSION = (0, 9, 7)
 __version__ = ".".join(map(str, VERSION))
 __author__ = "Ask Solem"
 __contact__ = "ask@celeryproject.org"
