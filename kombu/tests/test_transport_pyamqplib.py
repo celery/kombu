27983aa776ebc77721ec9353af568368c0fd3ab4
Ask Solem <ask@celeryproject.org>
2011-01-19 13:44:39 +0100

    Tests passes again.  Thanks to salmon_

--- kombu/tests/test_transport_pyamqplib.py
+++ kombu/tests/test_transport_pyamqplib.py
@@ -4,6 +4,12 @@ from kombu.transport import pyamqplib
 from kombu.connection import BrokerConnection
 
 
+class MockConnection(dict):
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
 class test_amqplib(unittest.TestCase):
 
     def test_conninfo(self):
@@ -17,7 +23,7 @@ class test_amqplib(unittest.TestCase):
     def test_default_port(self):
 
         class Transport(pyamqplib.Transport):
-            Connection = dict
+            Connection = MockConnection
 
         c = BrokerConnection(port=None, transport=Transport).connect()
         self.assertEqual(c["host"],
@@ -26,7 +32,7 @@ class test_amqplib(unittest.TestCase):
     def test_custom_port(self):
 
         class Transport(pyamqplib.Transport):
-            Connection = dict
+            Connection = MockConnection
 
         c = BrokerConnection(port=1337, transport=Transport).connect()
         self.assertEqual(c["host"], "localhost:1337")
