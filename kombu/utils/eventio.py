b7b625e0d8f7db96b53a8f3bc4b47557ce67d6de
Ask Solem <ask@celeryproject.org>
2011-01-28 11:49:10 +0100

    eventio.epoll: Don't convert None to float

--- kombu/utils/eventio.py
+++ kombu/utils/eventio.py
@@ -25,7 +25,7 @@ class _epoll(object):
             pass
 
     def poll(self, timeout):
-        return self._epoll.poll(timeout / 1000.0)
+        return self._epoll.poll(timeout and timeout / 1000.0)
 
 
 class _kqueue(object):
