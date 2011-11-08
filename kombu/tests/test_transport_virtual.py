31587c50f50e9612f70fe3d9962cfe5160f744fd
Ask Solem <ask@celeryproject.org>
2011-11-08 17:19:49 +0000

    Catch warnings in unittests

--- kombu/tests/test_transport_virtual.py
+++ kombu/tests/test_transport_virtual.py
@@ -1,9 +1,12 @@
 from __future__ import absolute_import
 
+import warnings
+
 from ..connection import BrokerConnection
 from ..transport import virtual
 from ..utils import uuid
 
+from .compat import catch_warnings
 from .utils import unittest
 from .utils import redirect_stdouts
 
@@ -323,8 +326,12 @@ class test_Channel(unittest.TestCase):
         self.assertEqual(self.channel._qos.prefetch_count, 128)
 
     def test_lookup__undeliverable(self, n="test_lookup__undeliverable"):
-        self.assertListEqual(self.channel._lookup(n, n, "ae.undeliver"),
-                             ["ae.undeliver"])
+        warnings.resetwarnings()
+        with catch_warnings(record=True) as log:
+            self.assertListEqual(self.channel._lookup(n, n, "ae.undeliver"),
+                                                      ["ae.undeliver"])
+            self.assertTrue(log)
+            self.assertIn("could not be delivered", log[0].message.args[0])
 
     def test_context(self):
         x = self.channel.__enter__()
