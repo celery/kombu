5914b4f2a385ff24703674df817563268f2ea470
Ask Solem <ask@celeryproject.org>
2012-01-13 17:36:54 +0000

    100% coverage for kombu.transport

--- kombu/tests/test_transport.py
+++ kombu/tests/test_transport.py
@@ -1,12 +1,30 @@
 from __future__ import absolute_import
+from __future__ import with_statement
+
+from mock import patch
 
 from .. import transport
-
 from .utils import unittest
 
 
 class test_transport(unittest.TestCase):
 
     def test_resolve_transport__no_class_name(self):
-        self.assertRaises(KeyError, transport.resolve_transport,
-                          "nonexistant")
+        with self.assertRaises(KeyError):
+            transport.resolve_transport("nonexistant")
+
+    def test_resolve_transport_when_callable(self):
+        self.assertTupleEqual(transport.resolve_transport(
+                lambda: "kombu.transport.memory.Transport"),
+                ("kombu.transport.memory", "Transport"))
+
+
+
+class test_transport_gettoq(unittest.TestCase):
+
+    @patch("warnings.warn")
+    def test_compat(self, warn):
+        x = transport._ghettoq("Redis", "redis", "redis")
+
+        self.assertEqual(x(), "kombu.transport.redis.Transport")
+        self.assertTrue(warn.called)
