468d80d9d982f3737cc228ca90a97b398cc198d5
Ask Solem <ask@celeryproject.org>
2012-03-20 14:27:38 +0000

    Hopefully closes #104

--- kombu/transport/SQS.py
+++ kombu/transport/SQS.py
@@ -154,7 +154,7 @@ class Channel(virtual.Channel):
 
     def entity_name(self, name, table=CHARS_REPLACE_TABLE):
         """Format AMQP queue name into a legal SQS queue name."""
-        return safe_str(name).translate(table)
+        return unicode(safe_str(name)).translate(table)
 
     def _new_queue(self, queue, **kwargs):
         """Ensures a queue exists in SQS."""
