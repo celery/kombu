0e7c4449d626b2d08ed7ba2648c210bca409d9a6
Ask Solem <ask@celeryproject.org>
2012-05-14 13:38:05 +0100

    Tests passing

--- kombu/tests/transport/test_sqlalchemy.py
+++ kombu/tests/transport/test_sqlalchemy.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import with_statement
 
 from mock import patch
+from nose import SkipTest
 
 from kombu.connection import BrokerConnection
 from kombu.tests.utils import TestCase
@@ -9,15 +10,19 @@ from kombu.tests.utils import TestCase
 
 class test_sqlalchemy(TestCase):
 
-    @patch("kombu.transport.sqlalchemy.Channel._open")
-    def test_url_parser(self, _open):
-        url = "sqlalchemy+sqlite://celerydb.sqlite"
-        BrokerConnection(url).connect()
-
-        url = "sqla+sqlite://celerydb.sqlite"
-        BrokerConnection(url).connect()
-
-        # Should prevent regression fixed by f187ccd
-        url = "sqlb+sqlite://celerydb.sqlite"
-        with self.assertRaises(KeyError):
+    def test_url_parser(self):
+        try:
+            import sqlalchemy
+        except ImportError:
+            raise SkipTest("sqlalchemy not installed")
+        with patch("kombu.transport.sqlalchemy.Channel._open"):
+            url = "sqlalchemy+sqlite://celerydb.sqlite"
             BrokerConnection(url).connect()
+
+            url = "sqla+sqlite://celerydb.sqlite"
+            BrokerConnection(url).connect()
+
+            # Should prevent regression fixed by f187ccd
+            url = "sqlb+sqlite://celerydb.sqlite"
+            with self.assertRaises(KeyError):
+                BrokerConnection(url).connect()
