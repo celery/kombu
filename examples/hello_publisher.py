f9fee64cc2997e61c6e8219ead5046642e8e38ce
Garrett Cooper <yanegomi@gmail.com>
2013-12-12 14:57:53 -0800

    Fix typo

--- examples/hello_publisher.py
+++ examples/hello_publisher.py
@@ -3,7 +3,7 @@ import datetime
 
 with Connection('amqp://guest:guest@localhost:5672//') as conn:
     simple_queue = conn.SimpleQueue('simple_queue')
-    message = 'helloword, sent at %s' % datetime.datetime.today()
+    message = 'hello world, sent at %s' % datetime.datetime.today()
     simple_queue.put(message)
     print('Sent: %s' % message)
     simple_queue.close()
