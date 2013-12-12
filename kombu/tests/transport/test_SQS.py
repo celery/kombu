2e7410ae0de2632676f28bc7605c1ebd4096eec8
Ask Solem <ask@celeryproject.org>
2013-12-12 17:28:31 +0000

    Tests should not use the filesystem

--- kombu/tests/transport/test_SQS.py
+++ kombu/tests/transport/test_SQS.py
@@ -8,7 +8,6 @@ slightly.
 from __future__ import absolute_import
 
 import os
-import pickle
 
 from kombu import Connection
 from kombu import messaging
@@ -25,162 +24,73 @@ except ImportError:
     SQS = None
 
 
-class SQSQueueMock:
-    name = None
+class SQSQueueMock(object):
 
-    def __init__(self, filename, create=False):
-        try:
-            open(filename + ".sqs")
-        except IOError:
-            if create:
-                open(filename + ".sqs", 'w')
-            else:
-                raise SyntaxError("Queue %s does not exist" % filename)
-        self.name = filename
+    def __init__(self, name):
+        self.name = name
+        self.messages = []
         self._get_message_calls = 0
 
     def clear(self, page_size=10, vtimeout=10):
-        try:
-            open(self.name + '.sqs', 'w').close()
-        except EOFError:
-            return False
-        return True
+        empty, self.messages[:] = not self.messages, []
+        return not empty
 
     def count(self, page_size=10, vtimeout=10):
-        try:
-            prev_data = pickle.load(open(self.name + '.sqs', 'r'))
-        except EOFError:
-            return 0
-        return len(prev_data)
-
-    def count_slow(self, page_size=10, vtimeout=10):
-        return self.count(page_size=page_size, vtimeout=vtimeout)
+        return len(self.messages)
+    count_slow = count
 
     def delete(self):
-        try:
-            os.remove(self.name + '.sqs')
-        except OSError:
-            # What happens here?
-            return False
+        self.messages[:] = []
         return True
 
     def delete_message(self, message):
-        prev_data = pickle.load(open(self.name + '.sqs', 'r'))
-        for data in prev_data:
-            if data.get_body() == message.get_body():
-                try:
-                    prev_data.remove(data)
-                    break
-                except ValueError:
-                    return False
         try:
-            pickle.dump(prev_data, open(self.name + '.sqs', 'w'))
-        except IOError:
+            self.messages.remove(message)
+        except ValueError:
             return False
-
         return True
 
     def get_messages(self, num_messages=1, visibility_timeout=None,
                      attributes=None, *args, **kwargs):
         self._get_message_calls += 1
-        messages = []
-        try:
-            prev_data = pickle.load(open(self.name + '.sqs', 'r'))
-        except EOFError:
-            prev_data = []
-        i = 0
-        while i < num_messages and len(prev_data) > 0:
-            try:
-                messages.append(prev_data[i])
-            except IndexError:
-                pass
-            i += 1
-        return messages
+        return self.messages[:num_messages]
 
     def read(self, visibility_timeout=None):
-        prev_data = pickle.load(open(self.name + '.sqs', 'r'))
-        try:
-            return prev_data.pop(0)
-        except IndexError:
-            # Is this the real action?
-            return None
+        return self.messages.pop(0)
 
     def write(self, message):
-        # Should do some error checking
-
-        # read in all the data in the queue first
-        try:
-            prev_data = pickle.load(open(self.name + '.sqs', 'r'))
-        except EOFError:
-            prev_data = []
-
-        prev_data.append(message)
-
-        try:
-            pickle.dump(prev_data, open(self.name + '.sqs', 'w'))
-        except IOError:
-            return False
-
+        self.messages.append(message)
         return True
 
 
-class SQSConnectionMock:
+class SQSConnectionMock(object):
+
+    def __init__(self):
+        self.queues = {}
+
     def get_queue(self, queue):
-        try:
-            open(queue + ".sqs")
-        except IOError:
-            return None
-        try:
-            return SQSQueueMock(queue)
-        except SyntaxError:
-            return None
+        return self.queues.get(queue)
 
     def get_all_queues(self, prefix=""):
-        queue_list = []
-        files = os.listdir(".")
-        for f in files:
-            if f[-4:] == '.sqs':
-                if prefix != "":
-                    if f[0:len(prefix)] == prefix:
-                        try:
-                            # Try to make the queue. If there's
-                            # something wrong, just move on.
-                            q = SQSQueueMock(f)
-                        except SyntaxError:
-                            continue
-                        queue_list.append(q)
-                else:
-                    try:
-                        # Try to make the queue. If there's something
-                        # wrong, just move on.
-                        q = SQSQueueMock(f[:-4])
-                    except SyntaxError:
-                        continue
-                    queue_list.append(q)
-        return queue_list
+        return self.queues.values()
 
     def delete_queue(self, queue, force_deletion=False):
         q = self.get_queue(queue)
-        if q.count() != 0:
-            # Can only delete empty queues
-            return False
-        return q.delete()
+        if q:
+            if q.count():
+                return False
+            q.clear()
+            self.queues.pop(queue, None)
 
     def delete_message(self, queue, message):
         return queue.delete_message(message)
 
     def create_queue(self, name, *args, **kwargs):
-        a = SQSQueueMock(name, create=True)
-        return a
+        q = self.queues[name] = SQSQueueMock(name)
+        return q
 
 
 class test_Channel(Case):
-    def removeMockedQueueFile(self, queue):
-        """Simple method to remove SQSQueueMock files"""
-        try:
-            os.remove('%s.sqs' % queue)
-        except OSError:
-            pass
 
     def handleMessageCallback(self, message):
         self.callback_message = message
@@ -191,6 +101,8 @@ class test_Channel(Case):
         # cannot execute our tests.
         if SQS is None:
             raise SkipTest('Boto is not installed')
+
+        SQS.Channel._queue_cache.clear()
 
         # Common variables used in the unit tests
         self.queue_name = 'unittest'
@@ -211,8 +123,7 @@ class test_Channel(Case):
 
         # Mock up a test SQS Queue with the SQSQueueMock class (and always
         # make sure its a clean empty queue)
-        self.removeMockedQueueFile(self.queue_name)
-        self.sqs_queue_mock = SQSQueueMock(self.queue_name, create=True)
+        self.sqs_queue_mock = SQSQueueMock(self.queue_name)
 
         # Now, create our Connection object with the SQS Transport and store
         # the connection/channel objects as references for use in these tests.
@@ -230,10 +141,6 @@ class test_Channel(Case):
                                    callback=self.handleMessageCallback,
                                    consumer_tag='unittest')
 
-    def tearDown(self):
-        """Clean up after ourselves"""
-        self.removeMockedQueueFile(self.queue_name)
-
     def test_init(self):
         """kombu.SQS.Channel instantiates correctly with mocked queues"""
         self.assertIn(self.queue_name, self.channel._queue_cache)
@@ -241,16 +148,14 @@ class test_Channel(Case):
     def test_new_queue(self):
         queue_name = "new_unittest_queue"
         self.channel._new_queue(queue_name)
-        self.assertTrue(os.path.exists('%s.sqs' % queue_name))
+        self.assertIn(queue_name, self.sqs_conn_mock.queues)
         # For cleanup purposes, delete the queue and the queue file
         self.channel._delete(queue_name)
-        self.removeMockedQueueFile(queue_name)
 
     def test_delete(self):
         queue_name = "new_unittest_queue"
         self.channel._new_queue(queue_name)
         self.channel._delete(queue_name)
-        self.removeMockedQueueFile(queue_name)
         self.assertNotIn(queue_name, self.channel._queue_cache)
 
     def test_get_from_sqs(self):
