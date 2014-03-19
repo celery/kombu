54198c62ed2d966bb16671e4dcc6934cdf05bc53
Ask Solem <ask@celeryproject.org>
2014-03-19 19:25:59 +0000

    Adds Trove classifier for Python 3.4

--- setup.py
+++ setup.py
@@ -157,6 +157,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 2.6',
