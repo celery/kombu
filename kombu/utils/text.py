698ea849b76a1dab6744b3c218ebb91904490dc0
Ask Solem <ask@celeryproject.org>
2014-04-15 17:53:41 +0100

    More version parser fixes.  Closes #340

--- kombu/utils/text.py
+++ kombu/utils/text.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from difflib import SequenceMatcher
 
 from kombu import version_info_t
+from kombu.five import string_t
 
 
 def fmatch_iter(needle, haystack, min_ratio=0.6):
@@ -25,8 +26,8 @@ def fmatch_best(needle, haystack, min_ratio=0.6):
 def version_string_as_tuple(s):
     v = _unpack_version(*s.split('.'))
     # X.Y.3a1 -> (X, Y, 3, 'a1')
-    if not v.releaselevel and v.micro:
-        v = version_info_t(v.major, v.minor, *_splitmicro(v.micro))
+    if isinstance(v.micro, string_t):
+        v = version_info_t(v.major, v.minor, *_splitmicro(*v[2:]))
     # X.Y.3a1-40 -> (X, Y, 3, 'a1', '40')
     if not v.serial and v.releaselevel and '-' in v.releaselevel:
         v = version_info_t(*list(v[0:3]) + v.releaselevel.split('-'))
@@ -37,10 +38,10 @@ def _unpack_version(major, minor=0, micro=0, releaselevel='', serial=''):
     return version_info_t(int(major), int(minor), micro, releaselevel, serial)
 
 
-def _splitmicro(s):
-    for index, char in enumerate(s):
+def _splitmicro(micro, releaselevel='', serial=''):
+    for index, char in enumerate(micro):
         if not char.isdigit():
             break
     else:
-        return s, '', ''
-    return s[:index], s[index:], ''
+        return int(micro or 0), releaselevel, serial
+    return int(micro[:index]), micro[index:], serial
