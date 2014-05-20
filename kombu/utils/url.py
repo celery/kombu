3dad9e7aac50293dc2628d9b6e93e9eec9e7ff0e
Ask Solem <ask@celeryproject.org>
2014-05-20 18:19:14 +0100

    parse_url function should not know about mongodb, and the transport already sets can_parse_url

--- kombu/utils/url.py
+++ kombu/utils/url.py
@@ -14,17 +14,9 @@ def _parse_url(url):
     schemeless = url[len(scheme) + 3:]
     # parse with HTTP URL semantics
     parts = urlparse('http://' + schemeless)
-
-    # The first pymongo.Connection() argument (host) can be
-    # a mongodb connection URI. If this is the case, don't
-    # use port but let pymongo get the port(s) from the URI instead.
-    # This enables the use of replica sets and sharding.
-    # See pymongo.Connection() for more info.
-    port = scheme != 'mongodb' and parts.port or None
-    hostname = schemeless if scheme == 'mongodb' else parts.hostname
     path = parts.path or ''
     path = path[1:] if path and path[0] == '/' else path
-    return (scheme, unquote(hostname or '') or None, port,
+    return (scheme, unquote(parts.hostname or '') or None, parts.port,
             unquote(parts.username or '') or None,
             unquote(parts.password or '') or None,
             unquote(path or '') or None,
