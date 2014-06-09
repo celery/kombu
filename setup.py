ef75f6a9237d4410f5597e3fd661df80d62acafa
Ask Solem <ask@celeryproject.org>
2014-06-09 16:10:30 +0100

    Define Python2.6 requirements for Wheels

--- setup.py
+++ setup.py
@@ -137,6 +137,8 @@ extras_require = extra['extras_require'] = {
     'slmq': extras('slmq.txt'),
 }
 
+extras_require[':python-version=="2.6"'] = reqs('py26.txt')
+
 setup(
     name='kombu',
     version=meta['VERSION'],
