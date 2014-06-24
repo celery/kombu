b0268c2220e867282366b7b6973a73cd12bc027f
Roger Hu <roger.hu@gmail.com>
2014-06-24 11:48:17 -0700

    Forgot one more can_cache_declaration declaration.
    The queues are still being cached because of the auto_delete=True.

--- kombu/entity.py
+++ kombu/entity.py
@@ -672,7 +672,7 @@ class Queue(MaybeChannelBound):
 
     @property
     def can_cache_declaration(self):
-        return True
+        return not self.auto_delete
 
     @classmethod
     def from_dict(self, queue, **options):
