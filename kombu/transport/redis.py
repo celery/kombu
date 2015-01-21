8cb30a3d85611cbeaa3a7c027144ef3efa0420bc
Alex Koshelev <daevaorn@gmail.com>
2015-01-21 16:16:38 +0300

    Fix message priority conversion for Redis and Beanstalk transports

--- kombu/transport/redis.py
+++ kombu/transport/redis.py
@@ -605,7 +605,7 @@ class Channel(virtual.Channel):
         queues = self._consume_cycle()
         if not queues:
             return
-        keys = [self._q_for_pri(queue, pri) for pri in PRIORITY_STEPS
+        keys = [self._q_for_pri(queue, pri) for pri in self.priority_steps
                 for queue in queues] + [timeout or 0]
         self._in_poll = True
         self.client.connection.send_command('BRPOP', *keys)
@@ -639,7 +639,7 @@ class Channel(virtual.Channel):
 
     def _get(self, queue):
         with self.conn_or_acquire() as client:
-            for pri in PRIORITY_STEPS:
+            for pri in self.priority_steps:
                 item = client.rpop(self._q_for_pri(queue, pri))
                 if item:
                     return loads(bytes_to_str(item))
@@ -648,7 +648,7 @@ class Channel(virtual.Channel):
     def _size(self, queue):
         with self.conn_or_acquire() as client:
             cmds = client.pipeline()
-            for pri in PRIORITY_STEPS:
+            for pri in self.priority_steps:
                 cmds = cmds.llen(self._q_for_pri(queue, pri))
             sizes = cmds.execute()
             return sum(size for size in sizes
@@ -664,7 +664,7 @@ class Channel(virtual.Channel):
 
     def _put(self, queue, message, **kwargs):
         """Deliver message."""
-        pri = self._get_message_priority(message)
+        pri = self._get_message_priority(message, reverse=True)
 
         with self.conn_or_acquire() as client:
             client.lpush(self._q_for_pri(queue, pri), dumps(message))
@@ -701,14 +701,14 @@ class Channel(virtual.Channel):
                                        pattern or '',
                                        queue or '']))
             cmds = client.pipeline()
-            for pri in PRIORITY_STEPS:
+            for pri in self.priority_steps:
                 cmds = cmds.delete(self._q_for_pri(queue, pri))
             cmds.execute()
 
     def _has_queue(self, queue, **kwargs):
         with self.conn_or_acquire() as client:
             cmds = client.pipeline()
-            for pri in PRIORITY_STEPS:
+            for pri in self.priority_steps:
                 cmds = cmds.exists(self._q_for_pri(queue, pri))
             return any(cmds.execute())
 
@@ -723,7 +723,7 @@ class Channel(virtual.Channel):
     def _purge(self, queue):
         with self.conn_or_acquire() as client:
             cmds = client.pipeline()
-            for pri in PRIORITY_STEPS:
+            for pri in self.priority_steps:
                 priq = self._q_for_pri(queue, pri)
                 cmds = cmds.llen(priq).delete(priq)
             sizes = cmds.execute()
