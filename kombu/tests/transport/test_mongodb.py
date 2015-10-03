a10b69e56be9c4a8bf0993dfbceb5ca64486f05f
Ask Solem <ask@celeryproject.org>
2015-10-02 17:45:32 -0700

    Fixes MongoDB test failure

--- kombu/tests/transport/test_mongodb.py
+++ kombu/tests/transport/test_mongodb.py
@@ -64,12 +64,12 @@ class test_mongodb(Case):
 
     @skip_if_not_module('pymongo')
     def test_options(self):
-        url = 'mongodb://localhost,localhost2:29017/dbname?safe=true'
+        url = 'mongodb://localhost,localhost2:29017/dbname?fsync=true'
         c = self._get_connection(url)
 
         hostname, dbname, options = c.channels[0]._parse_uri()
 
-        self.assertEqual(options['safe'], True)
+        self.assertTrue(options['fsync'])
 
     @skip_if_not_module('pymongo')
     def test_real_connections(self):
