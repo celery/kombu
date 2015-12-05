a4126d4bebff05b2878f6fb4836439b0b9f6ec84
wanglei <fatelei@gmail.com>
2015-12-05 23:16:16 +0800

    Add test

--- kombu/transport/beanstalk.py
+++ kombu/transport/beanstalk.py
@@ -12,10 +12,9 @@ from __future__ import absolute_import
 
 import socket
 
-from anyjson import loads, dumps
-
 from kombu.five import Empty
 from kombu.utils.encoding import bytes_to_str
+from kombu.utils.json import loads, dumps
 
 from . import virtual
 
