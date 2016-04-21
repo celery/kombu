59b446f0b90e5f1067f5e31d7a99ec55626c06b8
Marco Braak <mbraak@ridethepony.nl>
2016-04-21 10:32:08 +0200

    Fix redis unix socket connection; https://github.com/celery/celery/issues/2903

--- kombu/transport/redis.py
+++ kombu/transport/redis.py
@@ -836,6 +836,11 @@ class Channel(virtual.Channel):
                     'connection_class': redis.UnixDomainSocketConnection,
                     'path': '/' + path,
                     'password': password}, **query)
+
+                connparams.pop('socket_connect_timeout')
+                connparams.pop('socket_keepalive')
+                connparams.pop('socket_keepalive_options')
+
             connparams.pop('host', None)
             connparams.pop('port', None)
         connparams['db'] = self._prepare_virtual_host(
