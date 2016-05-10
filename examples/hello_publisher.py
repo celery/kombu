78cda4c410a22ece3520f03744b9020e23d5d482
xiaoshaaming <793053286@qq.com>
2016-05-10 11:13:56 +0800

    spelling mistake

--- examples/hello_publisher.py
+++ examples/hello_publisher.py
@@ -7,7 +7,7 @@ from kombu import Connection
 
 with Connection('amqp://guest:guest@localhost:5672//') as conn:
     simple_queue = conn.SimpleQueue('simple_queue')
-    message = 'helloword, sent at {0}'.format(datetime.datetime.today())
+    message = 'helloworld, sent at {0}'.format(datetime.datetime.today())
     simple_queue.put(message)
     print('Sent: {0}'.format(message))
     simple_queue.close()
