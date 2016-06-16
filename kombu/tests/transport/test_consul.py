9069315dc9a83f8df74fe947df53902778f97b97
Ask Solem <ask@celeryproject.org>
2016-06-15 19:21:01 -0700

    Some mostly cosmetic changes to #584

--- kombu/tests/transport/test_consul.py
+++ kombu/tests/transport/test_consul.py
@@ -1,35 +1,34 @@
 from __future__ import absolute_import, unicode_literals
 
-from kombu.tests.case import Case, Mock, skip
-from kombu.transport.consul import Channel
 from kombu.five import Empty
+
+from kombu.transport.consul import Channel, Transport
+
+from kombu.tests.case import Case, Mock, skip
 
 
 @skip.unless_module('consul')
-class TestConsul(Case):
-
-    mock_consul = None
-    channel = None
+class test_Consul(Case):
 
     def setup(self):
-        self.mock_consul = Mock()
-        self.mock_consul.kv = Mock()
-        self.mock_consul.session = Mock()
-        self.mock_consul.agent = Mock()
-
-        self.channel = Channel()
-        self.channel.consul = self.mock_consul
+        self.connection = Mock()
+        self.connection.client.transport_options = {}
+        self.connection.client.port = 303
+        self.consul = self.patch('consul.Consul').return_value
+        self.channel = Channel(connection=self.connection)
 
     def test_driver_version(self):
-        self.assertTrue(self.channel.transport.driver_version())
+        self.assertTrue(Transport(self.connection.client).driver_version())
 
     def test_failed_get(self):
-        self.channel._obtain_lock = Mock(return_value=False)
-        self.assertRaises(Empty, self.channel._get('empty'))
+        self.channel._acquire_lock = Mock(return_value=False)
+        self.channel.client.kv.get.return_value = (1, None)
+        with self.assertRaises(Empty):
+            self.channel._get('empty')()
 
     def test_test_purge(self):
         self.channel._destroy_session = Mock(return_value=True)
-        self.mock_consul.kv.delete = Mock(return_value=True)
+        self.consul.kv.delete = Mock(return_value=True)
         self.assertTrue(self.channel._purge('foo'))
 
     def test_variables(self):
@@ -47,44 +46,38 @@ class TestConsul(Case):
     def test_get_or_create_session(self):
         queue = 'myqueue'
         session_id = '123456'
-        self.mock_consul.session.create = Mock(return_value=session_id)
-        self.assertEqual(self._get_or_create_session(queue), session_id)
+        self.consul.session.create.return_value = session_id
+        self.assertEqual(
+            self.channel._get_or_create_session(queue),
+            session_id,
+        )
 
     def test_create_delete_queue(self):
-        channel = Channel()
-        channel.consul = self.mock_consul
-
         queue = 'mynewqueue'
 
-        self.mock_consul.kv.put = Mock(return_value=True)
-        self.assertTrue(channel._new_queue(queue))
+        self.consul.kv.put.return_value = True
+        self.assertTrue(self.channel._new_queue(queue))
 
-        self.mock_consul.kv.delete = Mock(return_value=True)
+        self.consul.kv.delete.return_value = True
         self.channel._destroy_session = Mock()
-        self._delete(queue)
+        self.channel._delete(queue)
 
     def test_size(self):
-        keys = list()
-        keys.append((1, dict()))
-        keys.append((2, dict()))
-
-        self.mock_consul.kv.get = Mock(return_value=keys)
-
-        self.assertEqual(self.channel._size(), 2)
+        self.consul.kv.get.return_value = [(1, {}), (2, {})]
+        self.assertEqual(self.channel._size('q'), 2)
 
     def test_get(self):
         self.channel._obtain_lock = Mock(return_value=True)
         self.channel._release_lock = Mock(return_value=True)
 
-        keys = list()
-        keys.append((1, dict()))
-        keys.append((2, dict()))
-        self.mock_consul.kv.get = Mock(return_value=keys)
+        self.consul.kv.get.return_value = [1, [
+            {'Key': 'myqueue', 'ModifyIndex': 1, 'Value': '1'},
+        ]]
 
-        self.mock_consul.kv.delete = Mock(return_value=True)
+        self.consul.kv.delete.return_value = True
 
         self.assertIsNotNone(self.channel._get('myqueue'))
 
     def test_put(self):
-        self.mock_consul.kv.put = Mock(return_value=True)
-        self.assertIsNone(self.channel.put('myqueue', 'mydata'))
+        self.consul.kv.put.return_value = True
+        self.assertIsNone(self.channel._put('myqueue', 'mydata'))
--- kombu/transport/consul.py
+++ kombu/transport/consul.py
@@ -4,39 +4,46 @@ Consul Kombu Transport
 It uses Consul.io's Key/Value store to transport messages in Queues
 
 It uses python-consul for talking to Consul's HTTP API
+
 """
 from __future__ import absolute_import, unicode_literals
 
 import uuid
 import socket
 
-from . import virtual
+from collections import defaultdict
+from contextlib import contextmanager
+
 from kombu.exceptions import ChannelError
-from kombu.log import get_logger
 from kombu.five import Empty, monotonic
+from kombu.log import get_logger
+from kombu.utils import cached_property
 from kombu.utils.json import loads, dumps
+
+from . import virtual
 
 try:
     import consul
 except ImportError:
     consul = None
 
-LOGGER = get_logger('kombu.transport.consul')
+logger = get_logger('kombu.transport.consul')
 
 DEFAULT_PORT = 8500
 DEFAULT_HOST = 'localhost'
 
 
+class LockError(Exception):
+    pass
+
+
 class Channel(virtual.Channel):
-    """
-    Consul Channel class which talks to the Consul Key/Value store
-    """
+    """Consul Channel class which talks to the Consul Key/Value store."""
+
     prefix = 'kombu'
     index = None
     timeout = '10s'
     session_ttl = 30
-    lock_name = '%s' % socket.gethostname()
-    queues = {}
 
     def __init__(self, *args, **kwargs):
         if consul is None:
@@ -47,103 +54,108 @@ class Channel(virtual.Channel):
         port = self.connection.client.port or self.connection.default_port
         host = self.connection.client.hostname or DEFAULT_HOST
 
-        LOGGER.debug('Host: %s Port: %d Timeout: %s', host, port, self.timeout)
+        logger.debug('Host: %s Port: %s Timeout: %s', host, port, self.timeout)
+
+        self.queues = defaultdict(dict)
 
         self.client = consul.Consul(host=host, port=int(port))
 
     def _lock_key(self, queue):
-        return '%s/%s.lock' % (self.prefix, queue)
+        return '{0}/{1}.lock'.format(self.prefix, queue)
 
     def _key_prefix(self, queue):
-        return '%s/%s' % (self.prefix, queue)
+        return '{0}/{1}'.format(self.prefix, queue)
 
     def _get_or_create_session(self, queue):
+        """Try to renew the session if it exists, otherwise create a new
+        session in Consul.
+
+        This session is used to acquire a lock inside Consul so that we achieve
+        read-consistency between the nodes.
+
+        :param queue: The name of the Queue.
+        :returns: The ID of the session.
+
         """
-        Try to renew the session if it exists, otherwise create a new session
-        in Consul
 
-        This session is used to obtain a lock inside Consul so that we achieve
-        read-consistency between the nodes
-
-        :param queue: The name of the Queue
-        :return: The ID of the session
-        """
-
-        session_id = None
         try:
             session_id = self.queues[queue]['session_id']
         except KeyError:
-            pass
-
-        if session_id is not None:
-            LOGGER.debug('Trying to renew existing session %s',
-                         self.queues[queue]['session_id'])
-            session = self.client.session.renew(session_id=session_id)
             session_id = None
-            try:
-                session_id = session['ID']
-            except KeyError:
-                pass
+        return (self._renew_existing_session(session_id)
+                if session_id is not None else self._create_new_session())
 
-        if session_id is None:
-            LOGGER.debug('Creating session %s with TTL %d', self.lock_name,
-                         self.session_ttl)
-            session_id = self.client.session.create(name=self.lock_name,
-                                                    ttl=self.session_ttl)
+    def _renew_existing_session(self, session_id):
+        logger.debug('Trying to renew existing session %s', session_id)
+        session = self.client.session.renew(session_id=session_id)
+        return session.get('ID')
 
-            LOGGER.debug('Created session %s with id %s', self.lock_name,
-                         session_id)
-
+    def _create_new_session(self):
+        logger.debug('Creating session %s with TTL %s',
+                     self.lock_name, self.session_ttl)
+        session_id = self.client.session.create(
+            name=self.lock_name, ttl=self.session_ttl)
+        logger.debug('Created session %s with id %s',
+                     self.lock_name, session_id)
         return session_id
 
-    def _obtain_lock(self, queue):
-        """
-        Try to obtain a lock on the Queue
+    @contextmanager
+    def _queue_lock(self, queue, raising=LockError):
+        """Try to acquire a lock on the Queue.
 
         It does so by creating a object called 'lock' which is locked by the
-        current session.
+        current session..
 
         This way other nodes are not able to write to the lock object which
-        means that they have to wait before the lock is released
+        means that they have to wait before the lock is released.
 
         :param queue: The name of the Queue
-        :return: True on success, False otherwise
+        :keyword raising: Set custom lock error class.
+        :raises LockError: if the lock cannot be acquired.
+        :returns: True on success, False otherwise.
+
         """
+        self._acquire_lock(queue, raising=raising)
+        try:
+            yield
+        finally:
+            self._release_lock(queue)
+
+    def _acquire_lock(self, queue, raising=LockError):
         session_id = self._get_or_create_session(queue)
         lock_key = self._lock_key(queue)
 
-        LOGGER.debug('Trying to create lock object %s with session %s',
+        logger.debug('Trying to create lock object %s with session %s',
                      lock_key, session_id)
 
         if self.client.kv.put(key=lock_key,
                               acquire=session_id,
-                              value=self.lock_name) is False:
-            LOGGER.info('Could not obtain a lock on key %s', lock_key)
-            return False
-
-        self.queues[queue]['session_id'] = session_id
-
-        return True
+                              value=self.lock_name):
+            self.queues[queue]['session_id'] = session_id
+            return
+        logger.info('Could not acquire lock on key %s', lock_key)
+        raise raising()
 
     def _release_lock(self, queue):
-        """
-        Try to release a lock. It does so by simply removing the lock key in
-        Consul.
+        """Try to release a lock. It does so by simply removing the lock key
+        in Consul.
 
         :param queue: The name of the queue we want to release the lock from
-        :return: None
+        :returns: None
+
         """
-        LOGGER.debug('Removing lock key %s', self._lock_key(queue))
+        logger.debug('Removing lock key %s', self._lock_key(queue))
         self.client.kv.delete(key=self._lock_key(queue))
 
     def _destroy_session(self, queue):
-        """
-        Destroy a previously created Consul session and release all locks
-        it still might hold
+        """Destroy a previously created Consul session and
+        release all locks it still might hold.
+
         :param queue: The name of the Queue
         :return: None
+
         """
-        LOGGER.debug('Destroying session %s', self.queues[queue]['session_id'])
+        logger.debug('Destroying session %s', self.queues[queue]['session_id'])
         self.client.session.destroy(self.queues[queue]['session_id'])
 
     def _new_queue(self, queue, **_):
@@ -152,83 +164,87 @@ class Channel(virtual.Channel):
 
     def _delete(self, queue, *args, **_):
         self._destroy_session(queue)
-        del self.queues[queue]
+        self.queues.pop(queue, None)
         self._purge(queue)
 
     def _put(self, queue, payload, **_):
-        """
-        Put `message` onto `queue`.
+        """Put `message` onto `queue`.
 
         This simply writes a key to the K/V store of Consul
-        """
-        key = '%s/msg/%d_%s' % (self._key_prefix(queue),
-                                int(round(monotonic() * 1000)),
-                                uuid.uuid4())
 
-        if self.client.kv.put(key=key, value=dumps(payload), cas=0) is False:
+        """
+        key = '{0}/msg/{1}_{2}'.format(
+            self._key_prefix(queue),
+            int(round(monotonic() * 1000)),
+            uuid.uuid4(),
+        )
+        if not self.client.kv.put(key=key, value=dumps(payload), cas=0):
             raise ChannelError('Cannot add key {0!r} to consul'.format(key))
 
     def _get(self, queue, timeout=None):
-        """
-        Get the first available message from the queue
+        """Get the first available message from the queue.
 
-        Before it does so it obtains a lock on the Key/Value store so
+        Before it does so it acquires a lock on the Key/Value store so
         only one node reads at the same time. This is for read consistency
+
         """
-        if self._obtain_lock(queue) is False:
-            raise Empty
+        with self._queue_lock(queue, raising=Empty):
+            key = '{0}/msg/'.format(self._key_prefix(queue))
+            logger.debug('Fetching key %s with index %s', key, self.index)
+            self.index, data = self.client.kv.get(
+                key=key, recurse=True,
+                index=self.index, wait=self.timeout,
+            )
 
-        key = '%s/msg/' % self._key_prefix(queue)
-        LOGGER.debug('Fetching key %s with index %s', key, self.index)
-        self.index, data = self.client.kv.get(key=key, recurse=True,
-                                              index=self.index,
-                                              wait=self.timeout)
+            try:
+                if data is None:
+                    raise Empty()
 
-        try:
-            if data is None:
-                raise Empty
+                logger.debug('Removing key %s with modifyindex %s',
+                             data[0]['Key'], data[0]['ModifyIndex'])
 
-            LOGGER.debug('Removing key %s with modifyindex %s',
-                         data[0]['Key'], data[0]['ModifyIndex'])
+                self.client.kv.delete(key=data[0]['Key'],
+                                      cas=data[0]['ModifyIndex'])
 
-            self.client.kv.delete(key=data[0]['Key'],
-                                  cas=data[0]['ModifyIndex'])
+                return loads(data[0]['Value'])
+            except TypeError:
+                pass
 
-            return loads(data[0]['Value'])
-        except TypeError:
-            pass
-        finally:
-            self._release_lock(queue)
-
-        raise Empty
+        raise Empty()
 
     def _purge(self, queue):
         self._destroy_session(queue)
-        return self.client.kv.delete(key='%s/msg/' % self._key_prefix(queue),
-                                     recurse=True)
+        return self.client.kv.delete(
+            key='{0}/msg/'.format(self._key_prefix(queue)),
+            recurse=True,
+        )
 
     def _size(self, queue):
         size = 0
         try:
-            key = '%s/msg/' % self._key_prefix(queue)
-            LOGGER.debug('Fetching key recursively %s with index %s', key,
-                         self.index)
-            self.index, data = self.client.kv.get(key=key, recurse=True,
-                                                  index=self.index,
-                                                  wait=self.timeout)
+            key = '{0}/msg/'.format(self._key_prefix(queue))
+            logger.debug('Fetching key recursively %s with index %s',
+                         key, self.index)
+            self.index, data = self.client.kv.get(
+                key=key, recurse=True,
+                index=self.index, wait=self.timeout,
+            )
             size = len(data)
         except TypeError:
             pass
 
-        LOGGER.debug('Found %d keys under %s with index %s', size, key,
-                     self.index)
+        logger.debug('Found %s keys under %s with index %s',
+                     size, key, self.index)
         return size
+
+    @cached_property
+    def lock_name(self):
+        return '{0}'.format(socket.gethostname())
 
 
 class Transport(virtual.Transport):
-    """
-    Consul K/V storage Transport for Kombu
-    """
+    """Consul K/V storage Transport for Kombu."""
+
     Channel = Channel
 
     default_port = DEFAULT_PORT
@@ -257,7 +273,7 @@ class Transport(virtual.Transport):
         port = connection.client.port or self.default_port
         host = connection.client.hostname or DEFAULT_HOST
 
-        LOGGER.debug('Verify Consul connection to %s:%d', host, port)
+        logger.debug('Verify Consul connection to %s:%s', host, port)
 
         try:
             client = consul.Consul(host=host, port=int(port))
