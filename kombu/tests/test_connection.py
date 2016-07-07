5262514e8f17a690ade22263e8b34b117221f064
Emmanuel Cazenave <contact@emcaz.fr>
2016-07-07 18:33:23 +0200

    Reproduce a deepcopy bug of a Connection instance

--- kombu/tests/test_connection.py
+++ kombu/tests/test_connection.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import pickle
 import socket
 
-from copy import copy
+from copy import copy, deepcopy
 
 from kombu import Connection, Consumer, Producer, parse_url
 from kombu.connection import Resource
@@ -137,6 +137,11 @@ class test_connection_utils(Case):
             userid='guest', password='guest', hostname='[::1]',
             port=5672, virtual_host='/',
         )
+
+    def test_connection_copy(self):
+        conn = Connection(self.url, alternates=['amqp://host'])
+        clone = deepcopy(conn)
+        self.assertEqual(clone.alt, ['amqp://host'])
 
 
 class test_Connection(Case):
