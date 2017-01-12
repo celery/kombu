1e4b1e3cbd2d07c68c21d70d6b35c503a5a19906
Jerry Seutter <jerry.seutter@rackspace.com>
2017-01-12 12:24:40 -0600

    Clean up imports

--- kombu/async/aws/ext.py
+++ kombu/async/aws/ext.py
@@ -3,9 +3,6 @@
 from __future__ import absolute_import, unicode_literals
 
 try:
-    import boto3
-
-    # TODO: old..
     import boto
 except ImportError:  # pragma: no cover
     boto = get_regions = ResultSet = RegionInfo = XmlHandler = None
@@ -20,21 +17,22 @@ except ImportError:  # pragma: no cover
     exception.SQSError = BotoError
     exception.SQSDecodeError = BotoError
 else:
-    from botocore import exceptions
-    # from boto3 import exceptions
-    from boto3 import session
-
-    # TODO: old..
     from boto import exception
     from boto.connection import AWSAuthConnection, AWSQueryConnection
     from boto.handler import XmlHandler
     from boto.resultset import ResultSet
     from boto.regioninfo import RegionInfo, get_regions
 
-__all__ = [
-    'exceptions',
 
-    # TODO: old..
-    'exception', 'AWSAuthConnection', 'AWSQueryConnection',
+try:
+    import boto3
+    from botocore import exceptions
+    from boto3 import session
+except ImportError:
+    pass
+
+
+__all__ = [
+    'exceptions','exception', 'AWSAuthConnection', 'AWSQueryConnection',
     'XmlHandler', 'ResultSet', 'RegionInfo', 'get_regions',
 ]
