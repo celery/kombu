69cf2d41a788b9bf483a4762db4e43158e8611b7
Lucas Li <lzl@uber.com>
2017-08-29 11:37:07 -0700

    update cherami transport unit test

--- t/unit/transport/test_cherami.py
+++ t/unit/transport/test_cherami.py
@@ -35,38 +35,20 @@ class test_CheramiChannel(unittest.TestCase):
         self.connection = Connection(transport=cherami.Transport)
         self.channel = self.connection.channel()
 
-        # set up the kombu async event loop object
-        self.channel.hub = Hub()
+        # set up async loop object
+        self.channel.hub = Mock()
 
         # set up mock cherami client publisher and consumer
         self.channel._publisher = Mock()
         self.channel._consumer = Mock()
 
-
-    def tearDown(self):
-        # Removes QoS reserved messages so we don't restore msgs on shutdown.
-        try:
-            qos = self.channel._qos
-        except AttributeError:
-            pass
-        else:
-            if qos:
-                qos._dirty.clear()
-                qos._delivered.clear()
-
     def test_basic_consume(self):
-        # make sure the first consume call registers the loop
-        self.channel._loop = Mock()
+        self.channel._get = Mock()
+        self.channel.prefetch_limit = 10
+        self.channel.prefetched = 5
 
         self.channel.basic_consume(self.queue_name, True, None, None)
-        self.channel._loop.assert_called_once_with(self.queue_name)
-
-    def test_loop(self):
-        # make sure the loop call does use hub to register event loop
-        self.channel.hub.call_repeatedly = Mock()
-
-        self.channel._loop(self.queue_name)
-        self.channel.hub.call_repeatedly.assert_called_once_with(self.channel.default_consume_interval, self.channel._get, self.queue_name)
+        self.channel.hub.call_soon.assert_called_once_with(self.channel._get, self.queue_name)
 
     def test_put(self):
         message = {'foo': 'bar'}
@@ -80,36 +62,45 @@ class test_CheramiChannel(unittest.TestCase):
         self.channel._publisher.publish.assert_called_with(str(0), dumps(message))
 
     def test_get(self):
-        delivery_token = 123456
+        delivery_token = '123456'
         message = CheramiMessage({'foo': 'bar'})
         results = [[delivery_token, message]]
         self.channel._consumer.receive = Mock(return_value=results)
         self.channel._on_message_ready = Mock()
+        self.channel.prefetched = 0
+        self.channel.prefetch_limit = 1
 
         self.channel._get(self.queue_name)
         self.channel._consumer.receive.assert_called_once_with(num_msgs=self.channel.default_message_batch_size)
         self.channel._on_message_ready.assert_called_once_with(self.queue_name, results)
 
+        # test prefetched > prefetch_limit
+        self.channel.prefetched = self.channel.prefetch_limit + 1
+        self.channel._get(self.queue_name)
+        # only called once like before because not allowed to consume
+        self.channel._consumer.receive.assert_called_once_with(num_msgs=self.channel.default_message_batch_size)
+        self.channel._on_message_ready.assert_called_once_with(self.queue_name, results)
+
     def test_on_message_ready(self):
-        self.channel._consumer.ack = Mock()
+        self.channel._get = Mock()
         self.channel._consumer.nack = Mock()
 
         # test regular message and ack
-        delivery_token = 123456
+        delivery_token = '123456'
         message = CheramiMessage({'foo': 'bar'})
         results = [[delivery_token, message]]
         self.channel._handle_message = Mock()
         self.channel._on_message_ready(self.queue_name, results)
-        self.channel._handle_message.assert_called_with(self.queue_name, {'foo': 'bar'})
-        self.channel._consumer.ack.assert_called_once_with(delivery_token)
+        self.channel._handle_message.assert_called_with(self.queue_name, {'foo': 'bar'}, delivery_token)
+        self.channel.hub.call_soon.assert_called_once_with(self.channel._get, self.queue_name)
 
         # test handle message exception and nack
-        delivery_token = 654321
+        delivery_token = '654321'
         message = CheramiMessage({'bar': 'foo'})
         results = [[delivery_token, message]]
         self.channel._handle_message = Mock(side_effect=Exception)
         self.channel._on_message_ready(self.queue_name, results)
-        self.channel._handle_message.assert_called_with(self.queue_name, {'bar': 'foo'})
+        self.channel._handle_message.assert_called_with(self.queue_name, {'bar': 'foo'}, delivery_token)
         self.channel._consumer.nack.assert_called_with(delivery_token)
 
         # test bad message format exception and nack
@@ -121,11 +112,25 @@ class test_CheramiChannel(unittest.TestCase):
 
     def test_handle_message(self):
         # make sure the callback is called
-        message = dumps(dict({'foo': 'bar'}))
-        self.channel.connection._callbacks[self.queue_name] = Mock()
+        message = dumps(dict({'foo': 'bar', 'properties': {'delivery_tag': '654321'}}))
+        self.channel.connection._deliver = Mock()
+        self.channel.prefetched = 0
+        delivery_token = '123456'
 
-        self.channel._handle_message(self.queue_name, message)
-        self.channel.connection._callbacks[self.queue_name].assert_called_once_with(loads(message))
+        self.channel._handle_message(self.queue_name, message, delivery_token)
+        assert self.channel.delivery_map['654321'] == '123456'
+        assert self.channel.prefetched == 1
+        self.channel.connection._deliver.assert_called_once_with(loads(message), self.queue_name)
+
+    def test_basic_ack(self):
+        self.channel._consumer.ack = Mock()
+        self.channel.delivery_map = dict({'654321': '123456'})
+        self.channel.prefetched = 1
+
+        self.channel.basic_ack('654321')
+        self.channel._consumer.ack.assert_called_once_with('123456')
+        assert len(self.channel.delivery_map) == 0
+        assert self.channel.prefetched == 0
 
     def test_close(self):
         self.channel._publisher.close = Mock()
