3758044989a4b5c52b3da7886b4091b45904da84
Lucas Li <lzl@uber.com>
2017-09-07 14:17:05 -0700

    revert back appveyor config

--- extra/appveyor/install.ps1
+++ extra/appveyor/install.ps1
@@ -76,59 +76,10 @@ function InstallPackage ($python_home, $pkg) {
     & $pip_path install $pkg
 }
 
-function InstallMissingHeaders () {
-    # Visual Studio 2008 is missing stdint.h, but you can just download one
-    # from the web.
-    # http://stackoverflow.com/questions/126279/c99-stdint-h-header-and-ms-visual-studio
-    $webclient = New-Object System.Net.WebClient
-
-    $include_dirs = @("C:\Program Files\Microsoft SDKs\Windows\v7.0\Include",
-                      "C:\Program Files\Microsoft SDKs\Windows\v7.1\Include",
-                      "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\include",
-                      "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include",
-                      "C:\Users\appveyor\AppData\Local\Programs\Common\Microsoft\Visual C++ for Python\9.0\VC\include")
-
-    Foreach ($include_dir in $include_dirs) {
-    $urls = @(@("https://raw.githubusercontent.com/chemeris/msinttypes/master/stdint.h", "stdint.h"),
-             @("https://raw.githubusercontent.com/chemeris/msinttypes/master/inttypes.h", "inttypes.h"))
-
-    Foreach ($i in $urls) {
-        $url = $i[0]
-        $filename = $i[1]
-
-        $filepath = "$include_dir\$filename"
-        if (Test-Path $filepath) {
-            Write-Host $filename "already exists in" $include_dir
-            continue
-        }
-
-        Write-Host "Downloading remedial " $filename " from" $url "to" $filepath
-        $retry_attempts = 2
-        for($i=0; $i -lt $retry_attempts; $i++){
-            try {
-                $webclient.DownloadFile($url, $filepath)
-                break
-            }
-            Catch [Exception]{
-                Start-Sleep 1
-            }
-       }
-
-       if (Test-Path $filepath) {
-           Write-Host "File saved at" $filepath
-       } else {
-           # Retry once to get the error message if any at the last try
-           $webclient.DownloadFile($url, $filepath)
-       }
-    }
-    }
-}
-
 function main () {
     InstallPython $env:PYTHON_VERSION $env:PYTHON_ARCH $env:PYTHON
     InstallPip $env:PYTHON
     InstallPackage $env:PYTHON wheel
-    InstallMissingHeaders
 }
 
 main
