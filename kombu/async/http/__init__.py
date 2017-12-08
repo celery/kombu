1c702ac936baeef9748f5fac75221338dc756d75
jay-johnson <jay.p.h.johnson@gmail.com>
2017-12-08 02:55:11 -0800

    SQS messages are moving from 'Messages Available' to 'Messages in Flight'

--- kombu/async/http/__init__.py
+++ kombu/async/http/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 
-from kombu.async import get_event_loop
+from kombu.async import get_event_loop, set_event_loop
+from kombu.async.hub import Hub
 
 from .base import Request, Headers, Response
 
@@ -17,7 +18,21 @@ def get_client(hub=None, **kwargs):
     """Get or create HTTP client bound to the current event loop."""
     hub = hub or get_event_loop()
     try:
-        return hub._current_http_client
+        if not hub:
+            hub = Hub()
+            set_event_loop(hub)
+
+        client = Client(hub, **kwargs)
+        return client
     except AttributeError:
-        client = hub._current_http_client = Client(hub, **kwargs)
+        
+        # What should happen on error?
+        # It appears to impact the message.ack() going
+        # back to SQS for issues like:
+        # https://github.com/celery/kombu/issues/746
+        if not hub:
+            hub = Hub()
+            set_event_loop(hub)
+
+        client = Client(hub, **kwargs)
         return client
