187891e06ab34820ec10b888c8cf19327771b42c
Alan Justino <alan.justino@yahoo.com.br>
2018-08-03 19:55:44 -0300

    Test hub.pooler regeneration

--- t/unit/asynchronous/test_hub.py
+++ t/unit/asynchronous/test_hub.py
@@ -239,6 +239,14 @@ class test_Hub:
         self.hub.close()
         poller.close.assert_called_with()
 
+    def test_poller_regeneration_on_access(self):
+        self.hub = Hub()
+        assert self.hub.poller
+        self.hub.stop()
+        self.hub.close()
+        assert self.hub._poller is None
+        assert self.hub.poller, 'It should be regenerated automatically!'
+
     def test_fire_timers(self):
         self.hub.timer = Mock()
         self.hub.timer._queue = []
