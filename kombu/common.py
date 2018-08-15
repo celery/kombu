7c7c008e2b8ff560f4774490a8231a3d0dabe139
chriskuan <chris@unnotech.com>
2018-08-15 17:05:13 +0800

    If give queue kwargs broadcast will fail
    Because give Broadcast(name='xxx', queue='xxx'),
    it will create one specific name for queue,
    and if launch more worker for consuming this queue,
    this specific name queue already exist then it will not create new queue to
    binding fanout exchange.
    So all worker will consuming same queue, and exchange only binding this
    queue then lose the effect of broadcasting.

--- kombu/common.py
+++ kombu/common.py
@@ -87,7 +87,7 @@ class Broadcast(Queue):
 
     def __init__(self, name=None, queue=None, auto_delete=True,
                  exchange=None, alias=None, **kwargs):
-        queue = queue or 'bcast.{0}'.format(uuid())
+        queue = '{0}.{1}'.format(queue or 'bcast', uuid())
         return super(Broadcast, self).__init__(
             alias=alias or name,
             queue=queue,
