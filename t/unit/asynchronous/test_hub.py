d55de2623624bba63aba4e15e60dafece25af58b
Mintu Kumar Sah <mintu.sah@joshtechnologygroup.com>
2018-10-07 20:18:08 +0530

    fixed test_poller_regeneration_on_access

--- t/unit/asynchronous/test_hub.py
+++ t/unit/asynchronous/test_hub.py
@@ -244,6 +244,7 @@ class test_Hub:
         self.hub = Hub()
         assert self.hub.poller
         self.hub.stop()
+        self.hub._ready = set()
         self.hub.close()
         assert self.hub._poller is None
         assert self.hub.poller, 'It should be regenerated automatically!'
