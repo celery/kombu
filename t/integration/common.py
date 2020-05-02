7c87441feb491370b3b8f65b6cb263268e26b8c2
Matus Valo <matusvalo@gmail.com>
2020-05-02 22:07:34 +0200

    Create common class of integration tests

---
+++ t/integration/common.py
@@ -0,0 +1,53 @@
+from __future__ import absolute_import, unicode_literals
+
+from contextlib import closing
+
+import kombu
+
+
+class BasicFunctionality(object):
+
+    def test_connect(self, connection):
+        connection.connect()
+        connection.close()
+
+    def test_publish_consume(self, connection):
+        test_queue = kombu.Queue('test', routing_key='test')
+
+        def callback(body, message):
+            assert body == {'hello': 'world'}
+            assert message.content_type == 'application/x-python-serialize'
+            message.delivery_info['routing_key'] == 'test'
+            message.delivery_info['exchange'] == ''
+            message.ack()
+            assert message.payload == body
+
+        with connection as conn:
+            with conn.channel() as channel:
+                producer = kombu.Producer(channel)
+                producer.publish(
+                    {'hello': 'world'},
+                    retry=True,
+                    exchange=test_queue.exchange,
+                    routing_key=test_queue.routing_key,
+                    declare=[test_queue],
+                    serializer='pickle'
+                )
+
+                consumer = kombu.Consumer(
+                    conn, [test_queue], accept=['pickle']
+                )
+                consumer.register_callback(callback)
+                with consumer:
+                    conn.drain_events(timeout=1)
+
+    def test_simple_publish_consume(self, connection):
+        with connection as conn:
+            with closing(conn.SimpleQueue('simple_test')) as queue:
+                queue.put({'Hello': 'World'}, headers={'k1': 'v1'})
+                message = queue.get(timeout=1)
+                assert message.payload == {'Hello': 'World'}
+                assert message.content_type == 'application/json'
+                assert message.content_encoding == 'utf-8'
+                assert message.headers == {'k1': 'v1'}
+                message.ack()
--- t/integration/test_redis.py
+++ t/integration/test_redis.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 
-from contextlib import closing
 import os
 
 import pytest
 import kombu
+
+from .common import BasicFunctionality
+
 
 def get_connection(
         hostname, port, vhost):
@@ -22,52 +24,8 @@ def connection(request):
         ).get("slaveid", None),
     )
 
-@pytest.mark.env('redis')
-@pytest.mark.flaky(reruns=5, reruns_delay=2)
-def test_connect(connection):
-    connection.connect()
-    connection.close()
 
 @pytest.mark.env('redis')
 @pytest.mark.flaky(reruns=5, reruns_delay=2)
-def test_publish_consume(connection):
-    test_queue = kombu.Queue('test', routing_key='test')
-
-    def callback(body, message):
-        assert body == {'hello': 'world'}
-        assert message.content_type == 'application/x-python-serialize'
-        message.delivery_info['routing_key'] == 'test'
-        message.delivery_info['exchange'] == ''
-        message.ack()
-        assert message.payload == body
-
-    with connection as conn:
-        with conn.channel() as channel:
-            producer = kombu.Producer(channel)
-            producer.publish(
-                {'hello': 'world'},
-                retry=True,
-                exchange=test_queue.exchange,
-                routing_key=test_queue.routing_key,
-                declare=[test_queue],
-                serializer='pickle'
-            )
-
-            consumer = kombu.Consumer(conn, [test_queue], accept=['pickle'])
-            consumer.register_callback(callback)
-            with consumer:
-                conn.drain_events(timeout=1)
-
-
-@pytest.mark.env('redis')
-@pytest.mark.flaky(reruns=5, reruns_delay=2)
-def test_simple_publish_consume(connection):
-    with connection as conn:
-        with closing(conn.SimpleQueue('simple_test')) as queue:
-            queue.put({'Hello': 'World'}, headers={'k1': 'v1'})
-            message = queue.get(timeout=1)
-            assert message.payload == {'Hello': 'World'}
-            assert message.content_type == 'application/json'
-            assert message.content_encoding == 'utf-8'
-            assert message.headers == {'k1': 'v1'}
-            message.ack()
+class test_BasicFunctionality(BasicFunctionality):
+    pass
