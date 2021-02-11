ae9ce769c3a51d19f101f83b6fbcb8087388bc43
raittes <raittes@gmail.com>
2021-02-11 14:34:45 -0300

    t/unit/asynchronous/http/test_curl: fix test_set_timeout

--- t/unit/asynchronous/http/test_curl.py
+++ t/unit/asynchronous/http/test_curl.py
@@ -95,8 +95,10 @@ class test_CurlClient:
             x._handle_socket(0xff3f, fd, x._multi, None, _pycurl)
 
     def test_set_timeout(self):
-        x = self.Client()
+        hub = Mock(name='hub')
+        x = self.Client(hub)
         x._set_timeout(100)
+        hub.call_later.assert_called_with(100, x._timeout_check)
 
     def test_timeout_check(self):
         with patch('kombu.asynchronous.http.curl.pycurl') as _pycurl:
