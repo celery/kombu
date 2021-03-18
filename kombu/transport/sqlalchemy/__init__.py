f48dba7e8f1540a1ef5184aaaf2452495a298152
Asif Saif Uddin (Auvi) <auvipy@gmail.com>
2021-03-18 12:21:02 +0600

    sqla 1.4 migration initialization

--- kombu/transport/sqlalchemy/__init__.py
+++ kombu/transport/sqlalchemy/__init__.py
@@ -69,7 +69,7 @@ from .models import (ModelBase, Queue as QueueBase, Message as MessageBase,
                      class_registry, metadata)
 
 
-VERSION = (1, 1, 0)
+VERSION = (1, 4, 0)
 __version__ = '.'.join(map(str, VERSION))
 
 _MUTEX = threading.RLock()
