e6d76d13fc342dec473828a7780c0c2f77b4b738
Luis Saavedra <571849+lsaavedr@users.noreply.github.com>
2021-05-22 15:28:05 -0400

    add accept parameter to SimpleQueue class (#1140)
    * add accept parameter to SimpleQueue class
    
    * Fixed missing accept for get_nowait() and added unittests
    
    * Remove unused **kwargs from SimpleQueue.__init__
    
    * Use self.consumer.accept instead of new attribute in SimpleQueue
    
    * Add tests for simple interface when accept=[]
    
    Co-authored-by: Matus Valo <matusvalo@gmail.com>

--- kombu/simple.py
+++ kombu/simple.py
@@ -67,7 +67,7 @@ class SimpleBase:
                 remaining = timeout - elapsed
 
     def get_nowait(self):
-        m = self.queue.get(no_ack=self.no_ack)
+        m = self.queue.get(no_ack=self.no_ack, accept=self.consumer.accept)
         if not m:
             raise self.Empty()
         return m
@@ -118,7 +118,7 @@ class SimpleQueue(SimpleBase):
 
     def __init__(self, channel, name, no_ack=None, queue_opts=None,
                  queue_args=None, exchange_opts=None, serializer=None,
-                 compression=None, **kwargs):
+                 compression=None, accept=None):
         queue = name
         queue_opts = dict(self.queue_opts, **queue_opts or {})
         queue_args = dict(self.queue_args, **queue_args or {})
@@ -134,13 +134,13 @@ class SimpleQueue(SimpleBase):
         else:
             exchange = queue.exchange
             routing_key = queue.routing_key
-        consumer = messaging.Consumer(channel, queue)
+        consumer = messaging.Consumer(channel, queue, accept=accept)
         producer = messaging.Producer(channel, exchange,
                                       serializer=serializer,
                                       routing_key=routing_key,
                                       compression=compression)
         super().__init__(channel, producer,
-                         consumer, no_ack, **kwargs)
+                         consumer, no_ack)
 
 
 class SimpleBuffer(SimpleQueue):
