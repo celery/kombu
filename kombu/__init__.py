b70e45da53bc6ae2639896715097ec0011ae0d41
Naomi Elstein <naomi.els@omerkatz.com>
2021-11-02 18:24:33 +0200

    Bump version: 5.2.0rc1 → 5.2.0

--- kombu/__init__.py
+++ kombu/__init__.py
@@ -5,7 +5,7 @@ import re
 import sys
 from collections import namedtuple
 
-__version__ = '5.2.0rc1'
+__version__ = '5.2.01'
 __author__ = 'Ask Solem'
 __contact__ = 'auvipy@gmail.com, ask@celeryproject.org'
 __homepage__ = 'https://kombu.readthedocs.io'
