900fde4a88446fafccea47a233bad9fe033c0d99
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-04-09 14:25:07 +0200

    Annotate `utils/uuid.py` and `utils/text.py`

--- t/unit/utils/test_uuid.py
+++ t/unit/utils/test_uuid.py
@@ -3,10 +3,10 @@ from kombu.utils.uuid import uuid
 
 class test_UUID:
 
-    def test_uuid4(self):
+    def test_uuid4(self) -> None:
         assert uuid() != uuid()
 
-    def test_uuid(self):
+    def test_uuid(self) -> None:
         i1 = uuid()
         i2 = uuid()
         assert isinstance(i1, str)
