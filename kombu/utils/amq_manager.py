e12b7c1354831ca1a154f0c139a2b8a2980d020b
khanh96le <khanh96le@gmail.com>
2022-05-30 18:38:26 +0700

    Annotate utils/amq_manager.py

--- kombu/utils/amq_manager.py
+++ kombu/utils/amq_manager.py
@@ -1,22 +1,28 @@
 """AMQP Management API utilities."""
 
-
 from __future__ import annotations
 
+from typing import Optional, Any
 
-def get_manager(client, hostname=None, port=None, userid=None,
-                password=None):
+from kombu import Connection
+
+DEFAULT_RABBIT_MQ_PORT = 15672
+
+
+def get_manager(client: Connection, hostname: Optional[str] = None,
+                port: Optional[int] = None, userid: Optional[str] = None,
+                password: Optional[str] = None):
     """Get pyrabbit manager."""
     import pyrabbit
     opt = client.transport_options.get
 
-    def get(name, val, default):
+    def get(name: str, val: Any, default: Any):
         return (val if val is not None
                 else opt('manager_%s' % name) or
-                getattr(client, name, None) or default)
+                     getattr(client, name, None) or default)
 
     host = get('hostname', hostname, 'localhost')
-    port = port if port is not None else opt('manager_port', 15672)
+    port = get('port', port, DEFAULT_RABBIT_MQ_PORT)
     userid = get('userid', userid, 'guest')
     password = get('password', password, 'guest')
     return pyrabbit.Client(f'{host}:{port}', userid, password)
