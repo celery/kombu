8ee1cff3f71e7f34e4d525c06897f86ff0e29d23
khanh96le <khanh96le@gmail.com>
2022-05-31 12:46:43 +0700

    Fix flake8

--- kombu/utils/amq_manager.py
+++ kombu/utils/amq_manager.py
@@ -9,17 +9,22 @@ from kombu import Connection
 DEFAULT_RABBITMQ_PORT = 15672
 
 
-def get_manager(client: Connection, hostname: str | None = None,
-                port: int | None = None, userid: str | None = None,
-                password: str | None = None):
+def get_manager(
+    client: Connection,
+    hostname: Optional[str] = None,
+    port: Optional[int] = None,
+    userid: Optional[str] = None,
+    password: Optional[str] = None
+):
     """Get pyrabbit manager."""
     import pyrabbit
     opt = client.transport_options.get
 
-    def get(name: str, val: Any, default: Any):
-        return (val if val is not None
-                else opt('manager_%s' % name) or
-                     getattr(client, name, None) or default)
+    def get(name: str, val: Any, default: Any) -> Any:
+        return (
+            val if val is not None
+            else opt('manager_%s' % name) or getattr(client, name, None) or default
+        )
 
     host = get('hostname', hostname, 'localhost')
     port = get('port', port, DEFAULT_RABBITMQ_PORT)
