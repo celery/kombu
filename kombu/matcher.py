6edc2b71539e8b003762e9e7105bfecf9e403453
Asif Saif Uddin <auvipy@gmail.com>
2022-06-21 10:01:37 +0600

    remove unused imports

--- kombu/matcher.py
+++ kombu/matcher.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from fnmatch import fnmatch
 from re import match as rematch
-from typing import Callable, Dict, Optional, cast
+from typing import Callable, cast
 
 from .utils.compat import entrypoints
 from .utils.encoding import bytes_to_str
@@ -24,7 +24,7 @@ class MatcherRegistry:
 
     def __init__(self) -> None:
         self._matchers: dict[str, MatcherFunction] = {}
-        self._default_matcher: MatcherFunction | None | None = None
+        self._default_matcher: MatcherFunction | None = None
 
     def register(self, name: str, matcher: MatcherFunction) -> None:
         """Add matcher by name to the registry."""
