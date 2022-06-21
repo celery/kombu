7b8467989fbbc4a0f9477306269cf45967278a12
Asif Saif Uddin <auvipy@gmail.com>
2022-06-21 10:30:16 +0600

    fix typing flake8 (#1560)
    * fix typing flake8
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove unused imports
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- kombu/matcher.py
+++ kombu/matcher.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from fnmatch import fnmatch
 from re import match as rematch
-from typing import Callable, Dict, Optional, cast
+from typing import Callable, cast
 
 from .utils.compat import entrypoints
 from .utils.encoding import bytes_to_str
