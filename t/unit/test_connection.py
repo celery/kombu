3215cf3828ad309f9391929a236f5c585b546c1b
Tomer Nosrati <tomer.nosrati@kcg.tech>
2022-12-15 17:00:14 +0200

    Added unit test: test_ensure_retry_errors_is_not_looping_infinitely()

--- t/unit/test_connection.py
+++ t/unit/test_connection.py
@@ -497,6 +497,20 @@ class test_Connection:
         with pytest.raises(OperationalError):
             ensured()
 
+    def test_ensure_retry_errors_is_not_looping_infinitely(self):
+        class _MessageNacked(Exception):
+            pass
+
+        def publish():
+            raise _MessageNacked('NACK')
+
+        with pytest.raises(ValueError):
+            self.conn.ensure(
+                self.conn,
+                publish,
+                retry_errors=(_MessageNacked,)
+            )
+
     def test_autoretry(self):
         myfun = Mock()
 
