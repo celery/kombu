9797ddf26b333d326d17cdd7840c866635158842
Paul Rysiavets <paul@covadem.com>
2024-10-08 16:26:47 +0200

    test(urllib3): add new tests

--- t/unit/asynchronous/http/test_urllib3.py
+++ t/unit/asynchronous/http/test_urllib3.py
@@ -4,6 +4,7 @@ from io import BytesIO
 from unittest.mock import Mock, patch
 
 import pytest
+import urllib3
 
 import t.skip
 from kombu.asynchronous.http.urllib3_client import Urllib3Client
@@ -13,22 +14,17 @@ from kombu.asynchronous.http.urllib3_client import Urllib3Client
 @pytest.mark.usefixtures('hub')
 class test_Urllib3Client:
     class Client(Urllib3Client):
-        Urllib3 = Mock(name='Urllib3')
+        urllib3 = Mock(name='urllib3')
 
     def test_max_clients_set(self):
         x = self.Client(max_clients=303)
         assert x.max_clients == 303
 
     def test_init(self):
-        with patch(
-                'kombu.asynchronous.http.urllib3_client.urllib3.PoolManager'
-        ) as _pool_manager:
-            x = self.Client()
-            assert x._http is not None
-            assert x._pending is not None
-            assert x._timeout_check_tref
-
-            _pool_manager.assert_called_with(maxsize=x.max_clients)
+        x = self.Client()
+        assert x._pools is not None
+        assert x._pending is not None
+        assert x._timeout_check_tref
 
     def test_close(self):
         with patch(
@@ -38,7 +34,8 @@ class test_Urllib3Client:
             x._timeout_check_tref = Mock(name='timeout_check_tref')
             x.close()
             x._timeout_check_tref.cancel.assert_called_with()
-            x._http.clear.assert_called_with()
+            for pool in x._pools.values():
+                pool.close.assert_called_with()
 
     def test_add_request(self):
         with patch(
@@ -71,7 +68,22 @@ class test_Urllib3Client:
                 method='GET',
                 url='http://example.com',
                 headers={},
-                body=None
+                body=None,
+                follow_redirects=True,
+                auth_username=None,
+                auth_password=None,
+                user_agent=None,
+                use_gzip=False,
+                network_interface=None,
+                validate_cert=True,
+                ca_certs=None,
+                client_cert=None,
+                client_key=None,
+                proxy_host=None,
+                proxy_port=None,
+                proxy_username=None,
+                proxy_password=None,
+                on_ready=Mock(name='on_ready')
             )
             response = Mock(
                 name='response',
@@ -102,14 +114,121 @@ class test_Urllib3Client:
             assert called_response.effective_url == response_obj.effective_url
             assert called_response.error == response_obj.error
 
+    def test_process_request_with_error(self):
+        with patch(
+                'kombu.asynchronous.http.urllib3_client.urllib3.PoolManager'
+        ) as _pool_manager:
+            x = self.Client()
+            x.pools_close()
+            request = Mock(
+                name='request',
+                method='GET',
+                url='http://example.com',
+                headers={},
+                body=None,
+                follow_redirects=True,
+                auth_username=None,
+                auth_password=None,
+                user_agent=None,
+                use_gzip=False,
+                network_interface=None,
+                validate_cert=True,
+                ca_certs=None,
+                client_cert=None,
+                client_key=None,
+                proxy_host=None,
+                proxy_port=None,
+                proxy_username=None,
+                proxy_password=None,
+                on_ready=Mock(name='on_ready')
+            )
+            _pool_manager.return_value.request.side_effect = urllib3.exceptions.HTTPError("Test Error")  # noqa
+
+            x._process_request(request)
+            request.on_ready.assert_called()
+            called_response = request.on_ready.call_args[0][0]
+            assert called_response.code == 599
+            assert called_response.error is not None
+            assert called_response.error.message == "Test Error"
+
     def test_on_readable_on_writable(self):
         x = self.Client()
         x.on_readable(Mock(name='fd'))
         x.on_writable(Mock(name='fd'))
 
-    def test_setup_request_sets_proxy_when_specified(self):
-        # FIXME: PoolManager or ProxyManager. not proxy per request.
+    def test_get_pool_with_proxy(self):
+        with patch(
+                'kombu.asynchronous.http.urllib3_client.urllib3.ProxyManager'
+        ) as _proxy_manager:
+            x = self.Client()
+            request = Mock(
+                name='request',
+                proxy_host='proxy.example.com',
+                proxy_port=8080,
+                proxy_username='user',
+                proxy_password='pass'
+            )
+            x.get_pool(request)
+            _proxy_manager.assert_called_with(
+                proxy_url='proxy.example.com:8080',
+                num_pools=x.max_clients,
+                proxy_headers=urllib3.make_headers(
+                    proxy_basic_auth="user:pass"
+                )
+            )
+
+    def test_get_pool_without_proxy(self):
         with patch(
                 'kombu.asynchronous.http.urllib3_client.urllib3.PoolManager'
-        ):
-            pass
+        ) as _pool_manager:
+            x = self.Client()
+            request = Mock(name='request', proxy_host=None)
+            x.get_pool(request)
+            _pool_manager.assert_called_with(num_pools=x.max_clients)
+
+    def test_process_request_with_proxy(self):
+        with patch(
+                'kombu.asynchronous.http.urllib3_client.urllib3.ProxyManager'
+        ) as _proxy_manager:
+            x = self.Client()
+            request = Mock(
+                name='request',
+                method='GET',
+                url='http://example.com',
+                headers={},
+                body=None,
+                follow_redirects=True,
+                proxy_host='proxy.example.com',
+                proxy_port=8080,
+                proxy_username='user',
+                proxy_password='pass',
+                on_ready=Mock(name='on_ready')
+            )
+            response = Mock(
+                name='response',
+                status=200,
+                headers={},
+                data=b'content'
+            )
+            response.geturl.return_value = 'http://example.com'
+            _proxy_manager.return_value.request.return_value = response
+
+            x._process_request(request)
+            response_obj = x.Response(
+                request=request,
+                code=200,
+                headers={},
+                buffer=BytesIO(b'content'),
+                effective_url='http://example.com',
+                error=None
+            )
+            request.on_ready.assert_called()
+            called_response = request.on_ready.call_args[0][0]
+            assert called_response.code == response_obj.code
+            assert called_response.headers == response_obj.headers
+            assert (
+                    called_response.buffer.getvalue()
+                    == response_obj.buffer.getvalue()
+            )
+            assert called_response.effective_url == response_obj.effective_url
+            assert called_response.error == response_obj.error
