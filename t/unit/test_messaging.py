087527f20967043d0e16dab1de1945e97bc59ffa
Omer Katz <omer.katz@kcg.tech>
2024-10-26 19:25:25 +0300

    Added confirm_timeout argument. (#2167)

--- t/unit/test_messaging.py
+++ t/unit/test_messaging.py
@@ -170,6 +170,14 @@ class test_Producer:
         timeout = p._channel.basic_publish.call_args[1]['timeout']
         assert timeout == 1
 
+    def test_publish_with_confirm_timeout(self):
+        p = self.connection.Producer()
+        p.channel = Mock()
+        p.channel.connection.client.declared_entities = set()
+        p.publish('test_timeout', exchange=Exchange('foo'), confirm_timeout=1)
+        confirm_timeout = p._channel.basic_publish.call_args[1]['confirm_timeout']
+        assert confirm_timeout == 1
+
     @patch('kombu.messaging.maybe_declare')
     def test_publish_maybe_declare_with_retry_policy(self, maybe_declare):
         p = self.connection.Producer(exchange=Exchange('foo'))
