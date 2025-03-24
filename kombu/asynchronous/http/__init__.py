22e5fe290f8283e5f8d56e8e4b3e00b3a747d2da
Paul Rysiavets <paul@covadem.com>
2025-03-24 15:32:55 +0100

    feat(pycurl): try importing pycurl when available and fall back to urllib3 when not

--- kombu/asynchronous/http/__init__.py
+++ kombu/asynchronous/http/__init__.py
@@ -3,22 +3,30 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from kombu.asynchronous import get_event_loop
-from kombu.asynchronous.http.base import Headers, Request, Response
+from kombu.asynchronous.http.base import BaseClient, Headers, Request, Response
 from kombu.asynchronous.hub import Hub
 
 if TYPE_CHECKING:
-    from kombu.asynchronous.http.curl import CurlClient
+    from kombu.asynchronous.http.base import BaseClient
 
-__all__ = ('Client', 'Headers', 'Response', 'Request')
+__all__ = ('Client', 'Headers', 'Response', 'Request', 'get_client')
 
 
-def Client(hub: Hub | None = None, **kwargs: int) -> CurlClient:
+def Client(hub: Hub | None = None, **kwargs: int) -> BaseClient:
     """Create new HTTP client."""
-    from .curl import CurlClient
-    return CurlClient(hub, **kwargs)
+    try:
+        import pycurl
+        from .curl import CurlClient
+        return CurlClient(hub, **kwargs)
+    except ImportError:
+        pass
+
+    # fall back scenario
+    from .urllib3_client import Urllib3Client
+    return Urllib3Client(hub, **kwargs)
 
 
-def get_client(hub: Hub | None = None, **kwargs: int) -> CurlClient:
+def get_client(hub: Hub | None = None, **kwargs: int) -> BaseClient:
     """Get or create HTTP client bound to the current event loop."""
     hub = hub or get_event_loop()
     try:
