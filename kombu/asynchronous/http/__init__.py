ba1506cfd7efdbd146e7045ee6042c044fe21a4e
Paul Rysiavets <paul@covadem.com>
2025-03-24 15:52:49 +0100

    ci: fix unused pycurl lint error

--- kombu/asynchronous/http/__init__.py
+++ kombu/asynchronous/http/__init__.py
@@ -14,15 +14,10 @@ __all__ = ('Client', 'Headers', 'Response', 'Request', 'get_client')
 
 def Client(hub: Hub | None = None, **kwargs: int) -> BaseClient:
     """Create new HTTP client."""
-    try:
-        import pycurl
-
-        from .curl import CurlClient
+    from .curl import CurlClient
+    if CurlClient.Curl is not None:
         return CurlClient(hub, **kwargs)
-    except ImportError:
-        pass
 
-    # fall back scenario
     from .urllib3_client import Urllib3Client
     return Urllib3Client(hub, **kwargs)
 
