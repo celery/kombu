e5ddc461fcdcf2a07b60d3de55ee635f4baca24c
Paul Rysiavets <paul@covadem.com>
2025-04-07 11:01:15 +0200

    fix(urllib3): revert urllib3

--- t/unit/asynchronous/http/test_http.py
+++ t/unit/asynchronous/http/test_http.py
@@ -147,7 +147,7 @@ class test_BaseClient:
 class test_Client:
 
     def test_get_client(self, hub):
-        pytest.importorskip('urllib3')
+        pytest.importorskip('pycurl')
         client = http.get_client()
         assert client.hub is hub
         client2 = http.get_client(hub)
