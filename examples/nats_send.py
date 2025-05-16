f5f3d1760a395f8d2e6d61630070922355a62f20
joeriddles <joeriddles10@gmail.com>
2025-05-16 09:11:29 -0600

    Add NATS demo server to examples

--- examples/nats_send.py
+++ examples/nats_send.py
@@ -1,12 +1,29 @@
 from __future__ import annotations
 
+import sys
+
+from nats.js.api import StorageType
+
 from kombu import Connection, Exchange, Queue
 
+LOCAL_SERVER = "localhost"
+DEMO_SERVER = "demo.nats.io"
+
+server = LOCAL_SERVER
+use_demo_server = len(sys.argv) > 1 and sys.argv[1] == "--demo"
+if use_demo_server:
+    server = DEMO_SERVER
+
+
 exchange = Exchange("exchange", "direct", durable=False)
-msg_queue = Queue("queue", exchange=exchange, routing_key="messages")
+msg_queue = Queue("kombu_demo", exchange=exchange, routing_key="messages")
 
 
-with Connection("nats://localhost:4222") as conn:
+with Connection(f"nats://{server}:4222", transport_options={
+    "stream_config": {
+        "storage": StorageType.FILE,
+    }
+}) as conn:
     producer = conn.Producer()
     producer.publish(
         "hello world", exchange=exchange, routing_key="messages", declare=[msg_queue]
